5ec823cb5642231de1467c93d327d533bd8857ae
Andrew Chon <andrewdchon@gmail.com>
2023-07-10 22:35:52 -0400

    feat: send JSON responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"andrew-chon/greenlight/internal/data"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,9 +15,20 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of the movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
